af1e177847908da488b759b79fb57e7a4c7f7b5e
Kaiyu Hsu <kaiyuhsu@user3-22-15.wireless.utoronto.ca>
2014-09-25 19:08:48 -0400

    main site

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('tedxUofT2015App', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
